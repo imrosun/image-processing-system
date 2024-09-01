5e86869da90f7247794eee84d82e29bbcae6f87b
imrosun <imrosun@gmail.com>
2024-09-01 23:50:37 +0530

    added env on vercel

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,12 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getMain(): any {
+    return {
+      name: 'image-processing-system',
+      version: '0.0.1',
+      description: 'An REST API with NESTJS and MONGODB',
+      author: 'imrosun',
+    };
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.enableCors();
-
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
+  await app.listen(3000);
 
   console.log(`Application is running on: ${await app.getUrl()}`);
   console.log("Database connected")
